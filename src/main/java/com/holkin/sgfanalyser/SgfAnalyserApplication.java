b42b015b21f61f670c236c721ac7e7b5a64c518b
victor.korniyenko <victor.korniyenko@victorkorniyenkos-MacBook-Pro.local>
2017-09-22 14:16:19 +0300

    initial

---
+++ src/main/java/com/holkin/sgfanalyser/SgfAnalyserApplication.java
@@ -0,0 +1,12 @@
+package com.holkin.sgfanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfAnalyserApplication.class, args);
+	}
+}
